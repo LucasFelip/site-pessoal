14e8429b12c9db0d9d5df777f043828ee3d197bb
Lucas Ferreira <75092912+LucasFelip@users.noreply.github.com>
2024-10-31 09:11:43 -0300

    [v1.0.7] Implementação de melhorias na interface e correção no carregamento de projetos (#57)
    - Ajustado o layout da seção "Sobre mim" para destacar o nome e função com imagem de fundo removido e estilizado para responsividade aprimorada.
    - Modificado o estilo da seção "Projetos Acadêmicos e Pessoais" para exibir os projetos de forma dinâmica, com opção de "Exibir mais projetos".
    - Corrigido o problema de carregamento dos projetos, garantindo que o botão "Exibir mais projetos" atualize a lista sem mostrar valores indefinidos.
    - Aplicada uma transição suave ao exibir novos projetos para uma experiência mais fluida e responsiva.
    - Adicionada acessibilidade ao botão "Ver mais" dos projetos, com aria-label para melhor suporte a leitores de tela.
    - Melhorias de responsividade e ajustes visuais para dispositivos móveis, aprimorando a experiência em diferentes tamanhos de tela.

--- assets/js/handlerCardHab.js
+++ assets/js/handlerCardHab.js
@@ -1,31 +1,15 @@
 const cardsHabs = [
     {
-        imagemLink: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg",
-        nome: "Python"
+        imagemLink: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg",
+        nome: "JavaScript"
     },
     {
         imagemLink: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg",
         nome: "Java"
     },
     {
-        imagemLink: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jira/jira-original.svg",
-        nome: "Jira"
-    },
-    {
-        imagemLink: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/swift/swift-original.svg",
-        nome: "Swift"
-    },
-    {
-        imagemLink: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg",
-        nome: "Bootstrap"
-    },
-    {
-        imagemLink: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/xcode/xcode-original.svg",
-        nome: "Xcode"
-    },
-    {
-        imagemLink: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/flutter/flutter-original.svg",
-        nome: "Flutter"
+        imagemLink: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/spring/spring-original.svg",
+        nome: "Spring Boot"
     },
     {
         imagemLink: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg",
@@ -36,82 +20,74 @@ const cardsHabs = [
         nome: "GitHub"
     },
     {
+        imagemLink: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/gitlab/gitlab-original.svg",
+        nome: "GitLab"
+    },
+    {
         imagemLink: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg",
         nome: "MySQL"
     },
     {
-        imagemLink: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-original.svg",
-        nome: "VSCode"
-    },
-    {
-        imagemLink: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/spring/spring-original.svg",
-        nome: "Spring"
-    },
-    {
-        imagemLink: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/intellij/intellij-original.svg",
-        nome: "IntelliJ"
-    },
-    {
-        imagemLink: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/premierepro/premierepro-original.svg",
-        nome: "Premiere"
+        imagemLink: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg",
+        nome: "PostgreSQL"
     },
     {
         imagemLink: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg",
         nome: "MongoDB"
     },
     {
-        imagemLink: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/figma/figma-original.svg",
-        nome: "Figma"
+        imagemLink: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-original.svg",
+        nome: "Docker"
     },
     {
-        imagemLink: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/c/c-original.svg",
-        nome: "C"
+        imagemLink: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg",
+        nome: "Bootstrap"
     },
     {
-        imagemLink: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/gitlab/gitlab-original.svg",
-        nome: "GitLab"
+        imagemLink: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/netlify/netlify-original.svg",
+        nome: "Netlify"
     },
     {
         imagemLink: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/firebase/firebase-plain.svg",
         nome: "Firebase"
     },
     {
-        imagemLink: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/xd/xd-plain.svg",
-        nome: "Adobre Xd"
+        imagemLink: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jira/jira-original.svg",
+        nome: "Jira"
+    },
+    {
+        imagemLink: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/amazonwebservices/amazonwebservices-plain-wordmark.svg",
+        nome: "AWS"
+    },
+    {
+        imagemLink: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-original.svg",
+        nome: "VSCode"
+    },
+    {
+        imagemLink: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/swift/swift-original.svg",
+        nome: "Swift"
+    },
+    {
+        imagemLink: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/xcode/xcode-original.svg",
+        nome: "Xcode"
+    },
+    {
+        imagemLink: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/flutter/flutter-original.svg",
+        nome: "Flutter"
     },
     {
         imagemLink: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/eclipse/eclipse-original.svg",
         nome: "Eclipse"
     },
     {
-        imagemLink: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/amazonwebservices/amazonwebservices-plain-wordmark.svg",
-        nome: "AWS"
-    },
-    {
-        imagemLink: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg",
-        nome: "Javascript"
-    },
-    {
-        imagemLink: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/docker/docker-original.svg",
-        nome: "Docker"
-    },
-    {
-        imagemLink: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/netlify/netlify-original.svg",
-        nome: "Netlify"
-    },
-    {
         imagemLink: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/postman/postman-original.svg",
         nome: "Postman"
     },
-    {
-        imagemLink: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tomcat/tomcat-original.svg",
-        nome: "Tomcat"
-    }
 ];
 
 function criarCard(card) {
     return `
-        <div class="card col-3 col-sm-1 m-1 handlerCardHab-exclusive">
+        <div class="col-3 col-sm-1 m-1 handlerCardHab-exclusive">
             <img src="${card.imagemLink}" />
         </div>
     `;
